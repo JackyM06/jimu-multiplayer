b5f19cf7db20407cf50c4da3af7b4b4d1c2a4685
maojiankai <maojiankai@kuaishou.com>
2023-04-07 19:32:42 +0800

    fix(signaling-server): WIP

--- packages/jimu-signaling-server/src/signaling/signaling.controller.ts
+++ packages/jimu-signaling-server/src/signaling/signaling.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, Redirect, Req } from '@nestjs/common';
 
 @Controller('signaling')
-export class SignalingController {}
+export class SignalingController {
+  @Get('check-health')
+  checkHealth(@Query('uid') uid: string, @Query('version') version: string) {
+    return uid + version;
+  }
+
+  @Get('check-version')
+  @Redirect('https://docs.nestjs.com', 302)
+  versionCheck(@Query('version') version: string) {
+    if (version && version === '5') {
+      return {
+        url: 'https://jimu.corp.kuaishou.com/',
+        statusCode: 302,
+      };
+    }
+  }
+}
